1e13a58fe82582869dc9605308ded63db0ed42f8
beautifulremi <98527099+beautifulrem@users.noreply.github.com>
2026-07-25 02:26:01 +0800

    fix: absorb closed LSP stdin errors

--- src/watchdog/lsp-diagnostics.ts
+++ src/watchdog/lsp-diagnostics.ts
@@ -268,6 +268,7 @@ class JsonRpcLspClient {
 		child.stderr.on("data", (chunk: Buffer) => {
 			this.stderr = `${this.stderr}${chunk.toString("utf-8")}`.slice(-MAX_STDERR_LENGTH);
 		});
+		child.stdin.on("error", (error) => this.rejectPending(error));
 		child.on("error", (error) => {
 			this.exited = true;
 			this.rejectPending(error);
